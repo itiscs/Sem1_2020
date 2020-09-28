0ac08026c57923d4f3ed13b232d7859030a8d2d2
moudroff <moudroff>
2020-09-28 22:57:51 +0300

    sin x^n

--- ExpApp/ExpApp/Program.cs
+++ ExpApp/ExpApp/Program.cs
@@ -10,26 +10,20 @@ namespace ExpApp
     {
         static void Main(string[] args)
         {
-            double x, exp, n, y, fact, stepen;
-            x = Convert.ToInt32(Console.ReadLine());
-            //n = Convert.ToInt32(Console.ReadLine());
-            exp = 0.0000001; 
-            y = 1;
-            fact = 1;
+            double x, n, y, stepen, t;
+            x = Convert.ToDouble(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            y = 0;
             stepen = 1;
-            int i=1;
-            //for (int i = 1; i <= n; i++)
-            while(stepen/fact >= exp)
+            t = -1;
+            for (int i = 1; i <= n; i++)
             {
-                fact *= i;
+                t *= -1;
                 stepen *= x;
-                y += stepen / fact;
-                i++;
+                y += t * (stepen / i);
             }
             Console.WriteLine(y);
-            Console.WriteLine(Math.Exp(x));
-            Console.WriteLine(i);
-            Math.Log(1 + x);
+            Console.WriteLine(Math.Log(1 + x));
         }
     }
 }
---
+++ NODApp/NODApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NODApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, z;
+            int n;
+            bool fl = false;
+            x = Convert.ToDouble(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            z = 1;
+            
+            for (int i = 0; i <= n; i++)
+            {
+                z *= x;
+                while (Math.Sin(z) < 0)
+                { 
+                    fl = true; break;
+                }
+
+            }
+            if (fl)
+                Console.WriteLine("да");
+            else
+                Console.WriteLine("нет");
+        }
+    }
+    
+}

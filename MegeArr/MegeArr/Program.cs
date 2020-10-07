30e70bc2a0858ab3725135f6f4238c1b45aa6a44
moudroff <moudroff>
2020-10-07 19:11:42 +0300

    Merge

---
+++ MegeArr/MegeArr/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegeArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 0, 2, 4, 5, 6, 8 };
+            bool fl = true;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] > a[i + 1])
+                {
+                    fl = false;
+                    break;
+                }
+                
+            }
+            if(fl)
+                Console.WriteLine("возрастающий массив");
+            else
+                Console.WriteLine("не возрастающий массив");
+
+
+
+        }
+    }
+}
+    

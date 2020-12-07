677ad155ef67dc1eb5293eee4e6a2c5c02b26165
moudroff <moudroff>
2020-12-07 17:22:01 +0300

    OOП

---
+++ FractionApp/FractionApp/Fraction.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionApp
+{
+    class Fraction
+    {
+        private int chisl;
+        private int znam;
+
+        public Fraction()
+        {
+            chisl = 0;
+            znam = 1;
+        }
+
+        public Fraction(int ch)
+        {
+            chisl = ch;
+            znam = 1;
+        }
+
+        public Fraction(int ch, int zn)
+        {
+            if(zn == 0)
+                throw new DivideByZeroException();
+
+            chisl = ch;
+            znam = zn;
+            Sokr();
+        }
+
+
+        public void Sokr()
+        {
+            var n = Nod(chisl, znam);
+            chisl /= n;
+            znam /= n;
+
+        }
+
+        private int Nod(int a, int b)
+        {
+            while (b != 0)
+                b = a % (a = b);
+            return a;
+        }
+
+        public void SetChisl(int c)
+        {
+            chisl = c;
+            Sokr();
+        }
+
+        public void SetZnam(int zn)
+        {
+            if (zn != 0)
+                znam = zn;
+            else
+                throw new DivideByZeroException();
+            Sokr();
+        }
+
+        public int GetZnam()
+        {
+            return znam;
+        }
+        public int GetChisl()
+        {
+            return chisl;
+        }
+
+        public void ShowFraction()
+        {
+            if (znam == 1)
+                Console.WriteLine($"{chisl}");
+            else
+                Console.WriteLine($"{chisl}/{znam}");
+        }
+
+        //создание новой дроби A + B 
+        public static Fraction Add(Fraction a, Fraction b)
+        {
+            var dr = new Fraction();
+            dr.chisl = b.chisl * a.znam + b.znam * a.chisl;
+            dr.znam = a.znam * b.znam;
+            dr.Sokr();
+            return dr;
+        }
+
+
+        //добавим параметр к this:   this = this + A 
+        public void Add(Fraction a)
+        {
+            chisl = chisl * a.znam + znam * a.chisl;
+            znam = znam * a.znam;
+            Sokr();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ FractionApp/FractionApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 4;
+
+            Fraction dr1 = new Fraction(8,8);
+
+            //dr1.SetChisl(4);
+            //dr1.SetZnam(5);
+
+            dr1.ShowFraction();
+
+            Fraction dr2 = new Fraction();
+
+            dr2.SetChisl(7);
+            dr2.SetZnam(9);
+
+            dr2.ShowFraction();
+
+
+
+            Fraction dr3 = new Fraction(5);
+            dr3.ShowFraction();
+
+
+            Fraction dr4 = new Fraction(3,6);
+            dr4.Sokr();
+            dr4.ShowFraction();
+
+
+            dr4.Add(dr2);
+            dr4.ShowFraction();
+
+
+            var dr6 = Fraction.Add(dr4, dr2);
+            dr6.ShowFraction();
+
+            
+
+
+
+        }
+    }
+}
